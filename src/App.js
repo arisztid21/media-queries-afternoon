00658a561c914c3fc2f76c698ab76eb7bae649c2
arisztid munoz <arisztidmunoz@arisztids-MacBook-Pro.local>
2018-09-03 15:15:17 -0700

    wpx7 initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+// import background from './background.png';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+      {/* <img className="background" src={background} alt="background"/> */}
+        <div className="big">
+        <button>button</button>
+        </div>
+        <div className="small">
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
